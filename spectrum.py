44e10604634b39af874df3260b0d6773f7f49a12
Simon Rogers <simon.d.rogers@gmail.com>
2020-04-03 13:44:32 +0100

    added hierarchy to spectrum object

--- spectrum.py
+++ spectrum.py
@@ -3,30 +3,23 @@
 
 import math
 
-class SpectralRecord(object):
-    def __init__(self,precursor_mz,peaks,metadata,original_file,spectrum_id):
+class Spectrum(object):
+    def __init__(self,precursor_mz,peaks):
         self.peaks = peaks
-        self.peaks.sort(key = lambda x: x[0]) # similarity scores assume sorting by m/z
-        self.spectrum_id = spectrum_id
-        self.metadata = metadata
-        self.original_file = original_file
+        self.peaks.sort(key = lambda x: x[0])
         self.precursor_mz = precursor_mz
         self._sqrt_normalise()
         self.n_peaks = len(self.peaks)
-        
-
+    
     def __str__(self):
-        if 'names' in self.metadata:
-            names = self.metadata['names'][0]
-        else:
-            names = ""
-        return ", ".join([self.spectrum_id,names,str(self.precursor_mz),self.original_file])
+        return srt(self.precursor_mz)
 
     def __repr__(self):
         return self.__str__()
 
     def __lt__(self,other):
         return(self.precursor_mz < other.precursor_mz)
+
 
     def _sqrt_normalise(self):
         temp = []
@@ -38,3 +31,19 @@ class SpectralRecord(object):
         self.normalised_peaks = []
         for mz,intensity in temp:
             self.normalised_peaks.append((mz,intensity/norm_facc))
+
+
+class SpectralRecord(Spectrum):
+    def __init__(self,precursor_mz,peaks,metadata,original_file,spectrum_id):
+        self.super().__init__(precursor_mz,peaks)
+        self.spectrum_id = spectrum_id
+        self.metadata = metadata
+        self.original_file = original_file        
+
+    def __str__(self):
+        if 'names' in self.metadata:
+            names = self.metadata['names'][0]
+        else:
+            names = ""
+        return ", ".join([self.spectrum_id,names,str(self.precursor_mz),self.original_file])
+
