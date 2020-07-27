67c5028e9e2dc21c7dbb7bc1ebed4201c8d3501e
Simon Rogers <simon.d.rogers@gmail.com>
2020-07-27 16:09:33 +0100

    modified setup.py for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mass_spec_utils", # Replace with your own username
-    version="0.0.5",
+    version="0.0.6",
     author="Simon Rogers",
     author_email="simon.d.rogers@gmail.com",
     description="Some useful MS code",
